06c8eadf61a91e1eb73631ba1f04b0efc93b7210
Fr1nge-Louis <37176227+Fr1nge-Louis@users.noreply.github.com>
2019-03-22 23:07:08 +0800

    修改配置等

--- src/main/java/cn/lhs/filesys/FilesysApplication.java
+++ src/main/java/cn/lhs/filesys/FilesysApplication.java
@@ -1,6 +1,7 @@
 package cn.lhs.filesys;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
@@ -15,20 +16,6 @@ public class FilesysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilesysApplication.class, args);
-	}
-
-	@Bean
-	MultipartConfigElement multipartConfigElement() {
-		//String url = "/opt/soft/myFileSystem/temp/";
-		String url = "F:/test/temp/";
-		File file = new File(url);
-		if (!file.exists()){
-			file.mkdir();
-		}
-
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-		factory.setLocation(url);
-		return factory.createMultipartConfig();
 	}
 
 }
--- src/main/java/cn/lhs/filesys/controller/FileUploadController.java
+++ src/main/java/cn/lhs/filesys/controller/FileUploadController.java
@@ -6,6 +6,7 @@ import cn.lhs.filesys.service.FileManageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,11 +29,13 @@ public class FileUploadController {
     private FileManageService fileManageService;
 
     private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
-    //private static final String imageUrl = "/opt/soft/myFileSystem/image/";
-    //private static final String videoUrl = "/opt/soft/myFileSystem/video/";
 
-    private static final String imageUrl = "F:/test/image/";
-    private static final String videoUrl = "F:/test/video/";
+
+    @Value("${image.url}")
+    private String imageUrl;
+
+    @Value("${video.url}")
+    private String videoUrl;
 
 
     @PostMapping("/image")
@@ -125,11 +128,11 @@ public class FileUploadController {
 
     }
 
-
+    //如果文件夹不存在，创建文件夹
     private void makeDir(String url){
         File file = new File(url);
-        if(!file.exists()){
-            file.mkdir();
+        while (!file.exists()){
+            file.mkdirs();
         }
     }
 }
